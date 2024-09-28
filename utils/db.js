1ea986ea5a78c2c6c0c0f29f7dbd7e9f43d18dec
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-09-28 20:17:56 +0300

    task#1-MongoDB

---
+++ utils/db.js
@@ -0,0 +1,48 @@
+import mongodb from 'mongodb';
+//  import Collection from 'mongodb/lib/collection';
+import envLoader from './env_loader';
+
+/**
+ * Represent a MongoDB Client.
+ */
+class DBClient {
+  /**
+   * Creates a new MongoDB Client.
+   */
+  constructor() {
+    envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.envDB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  /**
+   * Checks if this client's connection to the MongoDB server is active.
+   */
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  /**
+   * Retrieves a refernce to the 'users' collection.
+   * @returns {Promise<Collection>}
+   */
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  /**
+   * Retrives a refernce to the 'files' collection.
+   * @returns {Promise<Collection>}
+   */
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
